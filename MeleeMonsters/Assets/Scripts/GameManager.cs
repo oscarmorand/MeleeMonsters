521a2026f3ba5cf2f00df4ece7a832bd786344cb
Oscar Morand <oscar.morand@epita.fr>
2021-02-06 21:49:12 +0100

    instanciate

---
+++ MeleeMonsters/Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class GameManager : MonoBehaviourPunCallbacks
+{
+    public GameObject playerPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (playerPrefab == null)
+        {
+            Debug.LogError("<Color=Red><a>Missing</a></Color> playerPrefab Reference. Please set it up in GameObject 'Game Manager'", this);
+        }
+        else
+        {
+            Debug.Log("instancié!");
+            PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ MeleeMonsters/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private PhotonView PV;
+
+    [SerializeField]
+    private CharacterController myCC;
+
+    public float movementSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(PV.IsMine)
+        {
+            BasicMovement();
+        }
+    }
+
+    void BasicMovement()
+    {
+        if(Input.GetKey(KeyCode.Z))
+        {
+            myCC.Move(transform.up * Time.deltaTime * movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            myCC.Move(-transform.right * Time.deltaTime * movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            myCC.Move(-transform.up * Time.deltaTime * movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            myCC.Move(transform.right * Time.deltaTime * movementSpeed);
+        }
+    }
+}
