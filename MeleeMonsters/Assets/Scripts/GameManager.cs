391ef9a55f912b32026a8afbfdeb7887934ae4b7
Oscartichaud <72707902+Oscartichaud@users.noreply.github.com>
2021-02-09 14:21:02 +0100

    Revert "Initialisation random du joueur avec soit avocat soit fantome"
    This reverts commit 88559952266084b5aba6d518a9e03f7437393c8d.

--- MeleeMonsters/Assets/Scripts/GameManager.cs
+++ MeleeMonsters/Assets/Scripts/GameManager.cs
@@ -1,23 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
 
-public class GameManager : MonoBehaviour
+public class GameManager : MonoBehaviourPunCallbacks
 {
-    public int monsterType;
-
-    public int MonsterType { get { return monsterType; } }
-
-    private void Awake()
-    {
-        DontDestroyOnLoad(this);
-        monsterType = Random.Range(0, 2);
-    }
+    public GameObject playerPrefab;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        if (playerPrefab == null)
+        {
+            Debug.LogError("<Color=Red><a>Missing</a></Color> playerPrefab Reference. Please set it up in GameObject 'Game Manager'", this);
+        }
+        else
+        {
+            Debug.Log("instancié!");
+            PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
+        }
     }
 
     // Update is called once per frame
