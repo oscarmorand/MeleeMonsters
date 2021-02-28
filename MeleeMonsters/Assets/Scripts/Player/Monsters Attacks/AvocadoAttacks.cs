8d9def2d76b8ee154283ac79aa891c0e1e134383
Kevin <kevin.zhu@epita.fr>
2021-02-28 21:13:22 +0100

    First attempt for Avocado Side G

---
+++ MeleeMonsters/Assets/Scripts/Player/Monsters Attacks/AvocadoAttacks.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvocadoAttacks : MonoBehaviour
+{
+    private PlayerAttacks playerAttacks;
+    private Animator animator;
+
+    public Transform attackPoint;
+    public float attackRange;
+    public LayerMask playerLayers;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        attackRange = 0.5f;
+     
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AvocadoSideG()
+    {
+        //Jouer animation 
+
+        // Detection des enemies à portée 
+        Collider2D[] hitPlayer = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, playerLayers);
+
+        
+        // Application des dégâts 
+        foreach (Collider2D player in hitPlayer)
+        {
+            
+            Debug.Log("Vous avez touché" + " " + player.name);
+            
+        }
+
+
+
+    }
+
+     void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+
+}
---
+++ MeleeMonsters/Assets/Scripts/Player/PlayerAttacks.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttacks : MonoBehaviour
+{
+    private AvocadoAttacks avocadoAttacks;
+    private PlayerScript playerScript;
+
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        avocadoAttacks = GetComponent<AvocadoAttacks>();
+        playerScript = GetComponent<PlayerScript>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+        // Attaque SideG 
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            SideG();
+        }
+
+
+    }
+
+    void SideG()
+    {
+        PlayerScript.Monsters monsterType = playerScript.monster;
+        
+        // Si l'attaquant est l'avocat 
+        if (monsterType == (PlayerScript.Monsters) 0)
+        {
+            avocadoAttacks.AvocadoSideG();
+            
+        }
+
+        // Si l'attaquant est le fantome 
+        if (monsterType == (PlayerScript.Monsters) 1)
+        {
+            Debug.Log("Attaque du fantome déclenchée");
+        }
+
+    }
+
+    void NeutralG()
+    {
+
+    }
+
+    void DownG()
+    {
+
+    }
+
+    void SideA()
+    {
+
+    }
+
+    void NeutralA()
+    {
+
+    }
+
+    void DownA()
+    {
+
+    }
+
+    public void AddPercentage(int damage)
+    {
+
+        playerScript.percentage += damage;
+
+    }
+
+
+
+}
