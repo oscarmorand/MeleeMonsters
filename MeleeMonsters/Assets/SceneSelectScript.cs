6c22f28e967d0522b1a5888221e962aef2b32418
Oscar Morand <oscar.morand@epita.fr>
2021-04-23 16:18:25 +0200

    test  photon ui pour Liane

---
+++ MeleeMonsters/Assets/SceneSelectScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class SceneSelectScript : MonoBehaviour
+{
+
+    public GameObject charaPrefab;
+
+    public List<GameObject> charas = new List<GameObject>();
+
+    private void Start()
+    {
+        PhotonNetwork.Instantiate(charaPrefab.name, new Vector3(), Quaternion.identity, 0);
+    }
+}
---
+++ MeleeMonsters/Assets/SyncChara.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UI;
+
+public class SyncChara : MonoBehaviour, IPunObservable
+{
+
+    public Text _text;
+    public List<GameObject> sprites;
+    public int spriteInt;
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if(stream.IsWriting)
+        {
+            stream.SendNext(_text.text);
+            stream.SendNext(spriteInt);
+        }
+        else
+        {
+            _text.text = (string)stream.ReceiveNext();
+            spriteInt = (int)stream.ReceiveNext();
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < sprites.Count; i++)
+        {
+            if (i == spriteInt)
+                sprites[i].SetActive(true);
+            else
+                sprites[i].SetActive(false);
+        }
+    }
+}
