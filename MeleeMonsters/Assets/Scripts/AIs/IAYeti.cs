47542aaac64e6b088c0ec6f1db3ead831bbc4d40
Ewan Lemonnier <ewan.lemonnier@epita.fr>
2021-06-18 19:40:53 +0200

    coucou

--- MeleeMonsters/Assets/Scripts/AIs/IAYeti.cs
+++ MeleeMonsters/Assets/Scripts/AIs/IAYeti.cs
@@ -20,44 +20,94 @@ public class IAYeti : IAAttacks
     {
         System.Random rd = new System.Random();
 
-        if (relativeSideY >= -1f)
-        {
-            if (relativeSideY < 0.5f && distanceX < 1.75f)
-                playerAttacks.IAExecuteAttack(PlayerAttacks.attackType.dG); //dG
-        }
         if (relativeSideY >= 0f)
         {
-            if (distanceX < 1.75f)
+            if (distanceX < 1f)
             {
-                if (relativeSideY < 1.5f)
-                    playerAttacks.IAExecuteAttack(PlayerAttacks.attackType.sG); //sG
-
                 if (relativeSideY < 1f)
-                    playerAttacks.IAExecuteAttack((PlayerAttacks.attackType)Random2(0, 1)); //sG / dG
-            }
-            if (distanceX < 0.75f)
-            {
-                if (relativeSideY < 2f)
-                    playerAttacks.IAExecuteAttack(PlayerAttacks.attackType.nG); //nG
+                    playerAttacks.IAExecuteAttack((PlayerAttacks.attackType)Random3(0, 1, 2)); //sG / dG / nG
 
                 if (relativeSideY < 1.5f)
                     playerAttacks.IAExecuteAttack((PlayerAttacks.attackType)Random2(0, 2)); //sG / nG
 
+                if (relativeSideY < 2f)
+                    playerAttacks.IAExecuteAttack(PlayerAttacks.attackType.nG); //nG
+
+                if (relativeSideY < 4f && rd.Next(0, 12) == 0) //1 chance sur 12
+                    playerAttacks.IAExecuteAttack(PlayerAttacks.attackType.nS); //nS
+            }
+            if (distanceX < 1.75f)
+            {
                 if (relativeSideY < 1f)
-                    playerAttacks.IAExecuteAttack((PlayerAttacks.attackType)Random3(0, 1, 2)); //sG / dG / nG
+                    playerAttacks.IAExecuteAttack((PlayerAttacks.attackType)Random2(0, 1)); //sG / dG
+
+                if (relativeSideY < 1.5f)
+                    playerAttacks.IAExecuteAttack(PlayerAttacks.attackType.sG); //sG
+            }
+            if (distanceX < 2.5f)
+            {
+                if (relativeSideY < 1f && rd.Next(0, 5) == 0) //1 chance sur 5
+                    playerAttacks.IAExecuteAttack(PlayerAttacks.attackType.dS); //dS
             }
         }
+        if (relativeSideY >= -1f)
+        {
+            if (relativeSideY < 0.5f && distanceX < 1.75f)
+                playerAttacks.IAExecuteAttack(PlayerAttacks.attackType.dG); //dG
 
-        if (distanceY < 0.5f && distanceX >= 4f && rd.Next(0, 7) == 0) //1 chance sur 7
-            playerAttacks.IAExecuteAttack(PlayerAttacks.attackType.nS); //nS
+            if (relativeSideY < 1f && distanceX < 2.5f && rd.Next(0, 5) == 0) //1 chance sur 5
+                playerAttacks.IAExecuteAttack(PlayerAttacks.attackType.dS); //dS
+        }
 
-        if (distanceY < 1f && distanceX >= 3f && rd.Next(0, 25) == 0) //1 chance sur 25
+        if (relativeSideY < 2.5f && distanceX >= 4f && rd.Next(0, 7) == 0) //1 chance sur 7
             playerAttacks.IAExecuteAttack(PlayerAttacks.attackType.sS); //sS
     }
 
     public override void UseAirAttacks()
     {
-        
+        System.Random rd = new System.Random();
+
+        if (relativeSideY >= 0f)
+        {
+            if (distanceX < 1f)
+            {
+                if (relativeSideY < 1f)
+                    playerAttacks.IAExecuteAttack((PlayerAttacks.attackType)Random3(0, 1, 2)); //sG / dG / nG
+
+                if (relativeSideY < 1.5f)
+                    playerAttacks.IAExecuteAttack((PlayerAttacks.attackType)Random2(0, 2)); //sG / nG
+
+                if (relativeSideY < 2f)
+                    playerAttacks.IAExecuteAttack(PlayerAttacks.attackType.nG); //nG
+
+                if (relativeSideY < 4f && rd.Next(0, 12) == 0) //1 chance sur 12
+                    playerAttacks.IAExecuteAttack(PlayerAttacks.attackType.nS); //nS
+            }
+            if (distanceX < 1.75f)
+            {
+                if (relativeSideY < 1f)
+                    playerAttacks.IAExecuteAttack((PlayerAttacks.attackType)Random2(0, 1)); //sG / dG
+
+                if (relativeSideY < 1.5f)
+                    playerAttacks.IAExecuteAttack(PlayerAttacks.attackType.sG); //sG
+            }
+            if (distanceX < 2.5f)
+            {
+                if (relativeSideY < 1f && rd.Next(0, 5) == 0) //1 chance sur 5
+                    playerAttacks.IAExecuteAttack(PlayerAttacks.attackType.dS); //dS
+            }
+        }
+        if (relativeSideY >= -1f)
+        {
+            if (relativeSideY < 0.5f && distanceX < 1.75f)
+                playerAttacks.IAExecuteAttack(PlayerAttacks.attackType.dG); //dG
+
+            if (relativeSideY < 1f && distanceX < 2.5f && rd.Next(0, 5) == 0) //1 chance sur 5
+                playerAttacks.IAExecuteAttack(PlayerAttacks.attackType.dS); //dS
+        }
+
+        if (relativeSideY < 2.5f && distanceX >= 4f && rd.Next(0, 7) == 0) //1 chance sur 7
+            playerAttacks.IAExecuteAttack(PlayerAttacks.attackType.sS); //sS
     }
 
     public override void UseGroundAttacksW()
