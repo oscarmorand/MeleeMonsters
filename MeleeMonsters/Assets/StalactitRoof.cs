4de2a0c0ecf5039c65246796c9702cc53a9f6b1e
Oscar Morand <oscar.morand@epita.fr>
2021-06-18 17:29:58 +0200

    plafond dangereux

---
+++ MeleeMonsters/Assets/StalactitRoof.cs
@@ -0,0 +1,24 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StalactitRoof : MonoBehaviour
+{
+
+    public float knockback;
+    public int damage;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Player" || collision.transform.tag == "IA")
+        {
+            PhotonView pVTarget = collision.GetComponent<PhotonView>();
+            float bonus = 1;
+
+            pVTarget.RPC("Eject", RpcTarget.All, Vector2.down, knockback, bonus);
+
+            pVTarget.RPC("TakeDamage", RpcTarget.All, damage);
+        }
+    }
+}
